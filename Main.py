c65be6a363246ed49ca0466d07f4ab498463818d
Luka Jovanovic <lukaj2501@gmail.com>
2021-12-31 09:13:03 -0500

    finalized snowman collision again

--- Main.py
+++ Main.py
@@ -88,14 +88,14 @@ def update(player, fireballs, small_snowballs, big_snowballs,campfire, first_sno
     for snowball in small_snowballs:
         snowball.draw(win, SMALL_SNOWBALL)
 
-    boulders[0].draw(win, player, rocks[0])
-    boulders[1].draw(win, player, rocks[1])
-
     for firstsnowman in first_snowmen:
         firstsnowman.draw(win, first_snomwan)
 
     for secondsnowman in second_snowmen:
         secondsnowman.draw(win, second_snowman)
+
+    boulders[0].draw(win, player, rocks[0])
+    boulders[1].draw(win, player, rocks[1])
 
     win.blit(trees, (0, 0))
 
